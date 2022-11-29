76c91a2cb624024780a2e2a620808d701e39ea59
aadarshsb <83934598+aadarshsb@users.noreply.github.com>
2022-11-30 01:10:28 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -27,7 +27,7 @@ int main() {
     alertInCelcius(400.5);
     assert(alertFailureCount==1);
     alertInCelcius(303.6);
-    assert(alertFailureCount ==2)
+    assert(alertFailureCount ==2);
     printf("%d alerts failed.\n", alertFailureCount);
     printf("All is well (maybe!)\n");
     return 0;
